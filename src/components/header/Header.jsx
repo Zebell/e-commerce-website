19155efde32c649c4d5329b4795d95667ef01a61
Zebell <endziarkilan@gmail.com>
2022-11-21 18:54:51 +0500

    link-changed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react"
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
 import "./header.css"
  
 
@@ -28,6 +28,7 @@ const Header = () => {
   const headerRef = useRef(null)
   const totalQuantity= useSelector(state=> state.cart.totalQuantity)
   const menuRef = useRef(null)
+  const navigate = useNavigate()
 
   const stickyHeaderFunc = () => {
     window.addEventListener("scroll", () => {
@@ -46,6 +47,10 @@ const Header = () => {
   })
 
   const menuToggle = () => menuRef.current.classList.toggle("active__menu")
+
+  const navigateToCart = ()=>{
+    navigate('/cart')
+  }
 
   return (
     <header className="header" ref={headerRef}>
@@ -85,7 +90,7 @@ const Header = () => {
                 <i class="ri-heart-line"></i>
                 <span className="badge">1</span>
               </span>
-              <span className="cart__icon">
+              <span className="cart__icon" onClick={navigateToCart}>
                 <i class="ri-shopping-bag-line"></i>
                 <span className="badge">{totalQuantity}</span>
               </span>
